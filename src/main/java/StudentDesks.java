38b318be39cbe4c095589a9d1fa34256481ac3e7
Brendan Sirls <brendan.142408@nv.ccsd.net>
2020-09-04 21:57:16 +0000

    Desks finished

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -7,12 +7,13 @@ class StudentDesks {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-        
-
+        int sGroupA = scanner.nextInt();
+        int sGroupB = scanner.nextInt();
+        int sGroupC = scanner.nextInt();
         /*
          *  your code goes here
          */
-
+        System.out.print((sGroupA%2 + sGroupA/2) + (sGroupB%2 + sGroupB/2) + (sGroupC%2 + sGroupC/2));
 
         // closing the scanner object
         scanner.close();
